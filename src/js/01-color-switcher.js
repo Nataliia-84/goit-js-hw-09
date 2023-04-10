8230a7588414161464d8fd583592e461f54be1a2
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-10 23:13:40 +0200

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
 
+
+const refs={
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+    bodyEl: document.querySelector('body'),
+}
+
+
+refs.buttonStart.addEventListener('click', onClickButtonStart);
+refs.buttonStop.addEventListener('click', onClickButtonStop);
+
+let timerId=null;
+refs.buttonStop.disabled= true;
+
+function onClickButtonStart(){
+    
+   refs.buttonStart.disabled=true;
+   refs.buttonStop.disabled=false;
+   timerId = setInterval(()=>{
+    refs.bodyEl.setAttribute('style',` background: ${getRandomHexColor()}`);
+    },1000)
+    
+
+}
+
+function onClickButtonStop(){
+    
+    refs.buttonStart.disabled=false;
+    refs.buttonStop.disabled=true;
+    clearInterval(timerId);
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
