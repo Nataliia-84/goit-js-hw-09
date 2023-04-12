ec030118dad27da94137e33bd3562b17b0120f83
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-12 18:52:24 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,9 +11,8 @@ const day=document.querySelector('[data-days]')
 const buttonStart=document.querySelector("[data-start]")
 const inputEl=document.querySelector('#datetime-picker')
 
-// buttonStart.disabled=true;
-// inputEl.value.disabled=true;
- 
+buttonStart.disabled=true;
+
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -21,15 +20,23 @@ const options = {
     
     minuteIncrement: 1,
     onClose(selectedDates) {
-       
-        console.log(selectedDates[0]);
+      const dateInput = selectedDates[0].getTime();
+      const currentDay = new Date().getTime()
+      
+      if(dateInput>currentDay){
+        buttonStart.disabled=false;
+        
+      }
+      else{
+         alert("Please choose a date in the future");
+      }
+        
       }
   };
 
 flatpickr(inputEl, options);
 
-// let selectedDates=inputEl.value
-// console.log(selectedDates)
+
 
 buttonStart.addEventListener('click', onClick)
 function onClick(){
@@ -55,21 +62,17 @@ function onClick(){
     
         console.log(`'${days}':${hours}:${minutes}:${seconds}`)
     
-        if(days==='00'  && hours==='00' && minutes==='00' && seconds==='00' || endDay<startDay )
+        if(days==='00'  && hours==='00' && minutes==='00' && seconds==='00')
         {clearInterval(timerId);
-            buttonStart.disabled=true;
-            alert("Please choose a date in the future")
             sec.textContent='00';
             min.textContent='00';
             hour.textContent='00';
             day.textContent='00';}
+             
 
     },1000)
     
 }
-
-
-
 
 
 function addLeadingZero(value){
@@ -95,129 +98,3 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let timerId=null;
-
-//
-
-// inputEl.addEventListener('input',onInput)
-
-// function onInput(){
-   
-
-// const dateinput=new Date(inputEl.value)
-// console.log(dateinput)
-// const date=new Date()
-// console.log(date)
-// console.log(dateinput.getTime()<date.getTime())
-// if(dateinput.getTime()<=date.getTime()){
-
-//     buttonStart.disabled=true;
-//     alert("Please choose a date in the future");
-    
-//    return;
-// } buttonStart.disabled=false;
-
-// };
-
-// buttonStart.addEventListener('click',onClickStartTimer)
-// buttonStart.disabled=false;
-
-// const timer2={
-
-//     start(currentDay){
-       
-//        timerId = setInterval(()=>{const startTimer= new Date(currentDay)-new Date();
-    
-//             const { days, hours, minutes, seconds }=convertMs(startTimer)
-//             sec.textContent=seconds;
-//             min.textContent=minutes;
-//             hour.textContent=hours;
-//             day.textContent=days;
-            
-//             },1000)
-       
-//     }
-
-// }
-
-
-
-
-  
-
-
-
-
-
-
-// function onClickStartTimer(){
-//     buttonStart.disabled=true;
-   
-//     timer2.start(inputEl.value)
-// }
-
-
-// function pad(value){
-//     return String(value).padStart(2,'0')
-// }
-
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = pad(Math.floor(ms / day));
-//   // Remaining hours
-//   const hours = pad(Math.floor((ms % day) / hour));
-//   // Remaining minutes
-//   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-//   // Remaining seconds
-//   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
